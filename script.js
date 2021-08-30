dcee7f5ad0397bb914f454308b52495384fef206
Aravindan-111 <aravindhan11195@gmail.com>
2021-08-30 14:40:30 +0530

    color added

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ btn_cat.addEventListener('click', get_random_cat);
 async function get_random_cat() {
   let a = await fetch('https://random.dog/woof.json');
   let b = await a.json();
+  let alt = 'Cat Image';
   cat.innerHTML = `
-  <img src = "${b.url}">
+  <img src = "${b.url}" alt=${alt}>
   `;
 }
