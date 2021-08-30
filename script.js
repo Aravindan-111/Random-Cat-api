012564083b48765819db74e66b3d7e07b97bb0fc
Aravindan-111 <aravindhan11195@gmail.com>
2021-08-30 12:37:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let cat = document.getElementById('cat');
+let btn_cat = document.getElementById('btn_cat');
+
+btn_cat.addEventListener('click', get_random_cat);
+
+async function get_random_cat() {
+  let a = await fetch('https://random.dog/woof.json');
+  let b = await a.json();
+  cat.innerHTML = `
+  <img src = "${b.url}">
+  `;
+}
